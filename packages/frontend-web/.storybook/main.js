1aa1c6aac7878f980656380304f2d5cf7781acba
Victor Basumatary <vlenceleuth@gmail.com>
2022-08-03 11:58:21 +0530

    chore(frontend-web): :heavy_plus_sign: Add @storybook/addon-postcss
    To support tailwindcss

--- packages/frontend-web/.storybook/main.js
+++ packages/frontend-web/.storybook/main.js
@@ -8,6 +8,20 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "storybook-addon-next",
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        cssLoaderOptions: {
+          // When you have splitted your css over multiple files
+          // and use @import('./other-styles.css')
+          importLoaders: 1,
+        },
+        postcssLoaderOptions: {
+          // When using postCSS 8
+          implementation: require('postcss'),
+        },
+      }
+    }
   ],
   framework: "@storybook/react",
   core: {
