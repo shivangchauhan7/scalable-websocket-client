98bacb0fc452c0c945b00f0e4aa0929fe7ba37dd
Victor Basumatary <vlenceleuth@gmail.com>
2022-08-08 18:33:40 +0530

    feat(frontend-web): :sparkles: Allow custom classes for heading component

--- packages/frontend-web/components/h.js
+++ packages/frontend-web/components/h.js
@@ -1,3 +1,9 @@
-export default function Heading({children}) {
-  return <h2 className="uppercase text-sm font-bold pb-5">{children}</h2>
+export default function Heading(props) {
+  const {className = '', ...otherProps} = props
+
+  return (
+    <h2
+      className={'uppercase text-sm font-bold pb-5 ' + className}
+      {...otherProps} />
+  )
 }
