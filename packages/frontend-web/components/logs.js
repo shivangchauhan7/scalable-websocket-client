2dcf216821fe1f8690faf5bfdffd5976bd397143
Victor Basumatary <vlenceleuth@gmail.com>
2022-08-08 12:43:39 +0530

    feat(frontend-web): :sparkles: Add logs panel component

---
+++ packages/frontend-web/components/logs.js
@@ -0,0 +1,41 @@
+/**
+ * Renders all the logs.
+ */
+export default function Logs(props) {
+  const {logs} = props
+  
+  return (
+    <div className="flex flex-col-reverse overflow-auto">
+        {
+          logs.map((log) => {
+            const {id, content, level, createdAt} = log
+            let levelColor = 'text-black'
+
+            if (level === 'warn') {
+              levelColor = 'text-orange-600'
+            }
+
+            if (level === 'error') {
+              levelColor = 'text-red-600'
+            }
+            
+            return (
+              <div key={id} className="flex group py-1 px-2 hover:bg-slate-200 text-xs">
+                <span className={'w-12 inline-block uppercase font-bold ' + levelColor}>
+                  {level}
+                </span>
+                
+                <span className="grow px-2 inline-block">
+                  {content}
+                </span>
+
+                <span className="inline-block text-transparent group-hover:text-black">
+                  {createdAt.toLocaleString()}
+                </span>
+              </div>
+            )
+          })
+        }
+    </div>
+  )
+}
